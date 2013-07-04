fhoefling/luaponte

Allow registration of classes and functions using an object
This allows use of module() without pollution of the global environment, e.g.

local mylib = require("mylib")

extern "C" int luaopen_mylib(lua_State* L)
{
    using namespace luaponte;

    object mylib = newtable(L);

    module(mylib)
    [
        …
    ];

    mylib.push(L);

    return 1;
}

luaponte/scope.hpp
@@ -15,6 +15,7 @@
 
 #include <luaponte/prefix.hpp>
 #include <luaponte/config.hpp>
+#include <luaponte/object.hpp>
 #include <luaponte/lua_include.hpp>
 #include <memory>
 
@@ -70,13 +71,18 @@ private:
 class LUAPONTE_API module_
 {
 public:
-    module_(lua_State* L_, char const* name);
+    module_(object const& table);
+    module_(lua_State* L, char const* name);
     void operator[](scope s);
 
 private:
-    lua_State* m_state;
-    char const* m_name;
+    object m_table;
 };
+
+inline module_ module(object const& table)
+{
+    return module_(table);
+}
 
 inline module_ module(lua_State* L, char const* name = 0)
 {

mrgreywater/luabind

Conflicts:
	luaponte/scope.hpp
	src/scope.cpp

luabind/scope.hpp
@@ -25,6 +25,7 @@
 
 #include <luabind/prefix.hpp>
 #include <luabind/config.hpp>
+#include <luabind/object.hpp>
 #include <luabind/lua_state_fwd.hpp>
 #include <memory>
 
@@ -84,13 +85,19 @@ namespace luabind {
     class LUABIND_API module_
     {
     public:
-        module_(lua_State* L_, char const* name);
+        module_(object const& table);
+        module_(lua_State* L, char const* name);
         void operator[](scope s);
 
     private:
-        lua_State* m_state;
-        char const* m_name;
+        object m_table;
     };
+
+    inline module_ module(object const& table)
+    {
+        return module_(table);
+    }
+
 
     inline module_ module(lua_State* L, char const* name = 0)
     {